3cd80f3fb933cce6e7bff45bc865281f1c95a159
Brian Antunes <brian.antunes@yahoo.com>
2026-01-08 18:36:18 -0500

    Testing console app

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.Write("Target Row? ");
+string row = Console.ReadLine();
+Console.Write("Target Column? ");
+string column = Console.ReadLine();
+Console.WriteLine("Deploy to:");
+int rowInt = Convert.ToInt32(row);
+int colInt = Convert.ToInt32(column);
+
+Console.WriteLine($"({rowInt + 1}, {column})");
+Console.WriteLine($"({row}, {colInt - 1})");
+Console.WriteLine($"({rowInt - 1}, {column})");
+Console.WriteLine($"({row}, {colInt + 1})");
+
